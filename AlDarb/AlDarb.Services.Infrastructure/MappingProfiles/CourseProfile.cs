8a699f4b5c6d3b25edec10ea2d14e67c24934619
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-27 01:44:28 +0100

    Adding CourseProfile for mapping Course Entity and DTO

---
+++ AlDarb/AlDarb.Services.Infrastructure/MappingProfiles/CourseProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using AlDarb.DTO;
+using AlDarb.Entities;
+
+
+namespace AlDarb.Services.Infrastructure.MappingProfiles
+{
+    public class CourseProfile : Profile
+    {
+        public CourseProfile()
+        {
+            CreateMap<Course, CourseDTO>().ReverseMap();
+        }
+    }
+}
