4faa89a93b951eb8ba56cc0a297c1d88bc32612c
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-27 01:36:42 +0100

    Adding Notification entty configuration for EF Core

--- AlDarb/AlDarb.DataAccess.EFCore/Configuration/System/NotificationConfig.cs
+++ AlDarb/AlDarb.DataAccess.EFCore/Configuration/System/NotificationConfig.cs
@@ -17,21 +17,15 @@ namespace AlDarb.DataAccess.EFCore.Configuration.System
         {
             base.Configure(builder);
 
-            builder.Property(obj => obj.ApplicationDate).IsRequired();
-            builder.Property(obj => obj.Status);
-            builder.Property(obj => obj.AcceptedDate);
+            builder.Property(obj => obj.DateTime).IsRequired();
+            builder.Property(obj => obj.Description).IsRequired();
+            builder.Property(obj => obj.RedirectUrl).IsRequired();
             builder.Property(obj => obj.IsDeleted).HasDefaultValue(false);
 
             builder
                 .HasOne(obj => obj.User)
                 .WithOne()
                 .HasForeignKey<User>(obj => obj.Id)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            builder
-                .HasOne(obj => obj.CourseSession)
-                .WithOne()
-                .HasForeignKey<CourseSession>(obj => obj.Id)
                 .OnDelete(DeleteBehavior.Cascade);
         }
     }
