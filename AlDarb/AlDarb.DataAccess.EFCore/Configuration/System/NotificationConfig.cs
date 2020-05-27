7817487d0923744c5341df7de781b398bc664870
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-27 01:35:19 +0100

    Adding ProgressTask entty configuration for EF Core

---
+++ AlDarb/AlDarb.DataAccess.EFCore/Configuration/System/NotificationConfig.cs
@@ -0,0 +1,38 @@
+﻿using AlDarb.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlDarb.DataAccess.EFCore.Configuration.System
+{
+    public class NotificationConfig : BaseEntityConfig<Notification>
+    {
+        public NotificationConfig() : base("Notifications")
+        {
+        }
+
+        public override void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(obj => obj.ApplicationDate).IsRequired();
+            builder.Property(obj => obj.Status);
+            builder.Property(obj => obj.AcceptedDate);
+            builder.Property(obj => obj.IsDeleted).HasDefaultValue(false);
+
+            builder
+                .HasOne(obj => obj.User)
+                .WithOne()
+                .HasForeignKey<User>(obj => obj.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasOne(obj => obj.CourseSession)
+                .WithOne()
+                .HasForeignKey<CourseSession>(obj => obj.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ AlDarb/AlDarb.DataAccess.EFCore/Configuration/System/ProgressTaskConfig.cs
@@ -0,0 +1,38 @@
+﻿using AlDarb.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlDarb.DataAccess.EFCore.Configuration.System
+{
+    public class ProgressTaskConfig : BaseEntityConfig<ProgressTask>
+    {
+        public ProgressTaskConfig() : base("ProgressTasks")
+        {
+        }
+
+        public override void Configure(EntityTypeBuilder<ProgressTask> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(obj => obj.progress).IsRequired();
+            builder.Property(obj => obj.ProgressDate).IsRequired();
+            builder.Property(obj => obj.rating);
+            builder.Property(obj => obj.IsDeleted).HasDefaultValue(false);
+
+            builder
+                .HasOne(obj => obj.User)
+                .WithOne()
+                .HasForeignKey<User>(obj => obj.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasOne(obj => obj.CourseTask)
+                .WithOne()
+                .HasForeignKey<CourseTask>(obj => obj.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
