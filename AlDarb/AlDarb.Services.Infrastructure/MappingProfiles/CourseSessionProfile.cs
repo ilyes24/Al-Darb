65d10e4a1952f596f04bf581a824f73447534590
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-27 01:46:14 +0100

    Adding CourseSessionProfile for mapping CourseSession Entity and DTO

---
+++ AlDarb/AlDarb.Services.Infrastructure/MappingProfiles/CourseSessionProfile.cs
@@ -0,0 +1,18 @@
+﻿using AlDarb.DTO;
+using AlDarb.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlDarb.Services.Infrastructure.MappingProfiles
+{
+    public class CourseSessionProfile : Profile
+    {
+        public CourseSessionProfile()
+        {
+            CreateMap<CourseSession, CourseSessionDTO>().ReverseMap();
+        }
+    }
+
+}
