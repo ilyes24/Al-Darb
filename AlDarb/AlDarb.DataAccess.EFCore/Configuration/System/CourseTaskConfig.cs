f4d853bd5104072d39321db1443a4f35f3adbef7
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-27 01:19:01 +0100

    Adding CourseTask entty configuration for EF Core

---
+++ AlDarb/AlDarb.DataAccess.EFCore/Configuration/System/CourseTaskConfig.cs
@@ -0,0 +1,33 @@
+﻿using AlDarb.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlDarb.DataAccess.EFCore.Configuration.System
+{
+    public class CourseTaskConfig : BaseEntityConfig<CourseTask>
+    {
+        public CourseTaskConfig() : base("CourseTasks")
+        {
+        }
+
+        public override void Configure(EntityTypeBuilder<CourseTask> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(obj => obj.Title).IsRequired();
+            builder.Property(obj => obj.Duration).IsRequired();
+            builder.Property(obj => obj.Description).IsRequired();
+            builder.Property(obj => obj.Difficulty).IsRequired();
+            builder.Property(obj => obj.IsDeleted).HasDefaultValue(false);
+
+            builder
+                .HasOne(obj => obj.Course)
+                .WithOne()
+                .HasForeignKey<Course>(obj => obj.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
