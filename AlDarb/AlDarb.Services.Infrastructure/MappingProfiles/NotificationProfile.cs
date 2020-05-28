29b7743c46d45077d1f3cadaa127bf796357732e
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-28 07:25:09 +0100

    Adding Profiles for mapping Entities and DTOs

---
+++ AlDarb/AlDarb.Services.Infrastructure/MappingProfiles/NotificationProfile.cs
@@ -0,0 +1,14 @@
+﻿using AlDarb.DTO;
+using AlDarb.Entities;
+using AutoMapper;
+
+namespace AlDarb.Services.Infrastructure.MappingProfiles
+{
+    public class NotificationProfile : Profile
+    {
+        public NotificationProfile()
+        {
+            CreateMap<Notification, NotificationDTO>().ReverseMap();
+        }
+    }
+}
---
+++ AlDarb/AlDarb.Services.Infrastructure/MappingProfiles/ProgressTaskProfile.cs
@@ -0,0 +1,14 @@
+﻿using AlDarb.DTO;
+using AlDarb.Entities;
+using AutoMapper;
+
+namespace AlDarb.Services.Infrastructure.MappingProfiles
+{
+    public class ProgressTaskProfile : Profile
+    {
+        public ProgressTaskProfile()
+        {
+            CreateMap<ProgressTask, ProgressTaskDTO>().ReverseMap();
+        }
+    }
+}
