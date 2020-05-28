4d857e8037cd40c92b461d2a5c2847402aa90f9c
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-28 08:12:46 +0100

    Implementing course repository

---
+++ AlDarb/AlDarb.DataAccess.EFCore/Repositories/CourseRepository.cs
@@ -0,0 +1,32 @@
+﻿using AlDarb.Entities;
+using AlDarb.Services.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlDarb.DataAccess.EFCore.Repositories
+{
+    public class CourseRepository : BaseDeletableRepository<Course, DataContext>, ICourseRepository<Course>
+    {
+        public CourseRepository(DataContext context) : base(context)
+        {
+        }
+
+        public async Task<Course> GetByName(string name, ContextSession session, bool includeDeleted = false)
+        {
+            return await GetEntities(session, includeDeleted)
+                .Where(obj => obj.Name == name)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<Course> GetByUserId(int userId, ContextSession session, bool includeDeleted = false)
+        {
+            return await GetEntities(session, includeDeleted)
+                .Where(obj => obj.User.Id == userId)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
