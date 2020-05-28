5c5a30c10a84a14e98abb2266caf402680c11b36
ilyes24 <ilyes24ghomrani@gmail.com>
2020-05-28 09:01:04 +0100

    Implementing course session repository

---
+++ AlDarb/AlDarb.DataAccess.EFCore/Repositories/CourseSessionRepository.cs
@@ -0,0 +1,37 @@
+﻿using AlDarb.Entities;
+using AlDarb.Services.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlDarb.DataAccess.EFCore.Repositories
+{
+    public class CourseSessionRepository : BaseDeletableRepository<CourseSession, DataContext>, ICourseSessionRepository<CourseSession>
+    {
+        public CourseSessionRepository(DataContext context) : base(context)
+        {
+        }
+
+        public  async Task<CourseSession> GetByCourseId(int courseId, ContextSession session, bool includeDeleted = false)
+        {
+            return await GetEntities(session, includeDeleted)
+                .Where(obj => obj.Course.Id == courseId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<CourseSession> GetByEndDate(DateTime endDate, ContextSession session, bool includeDeleted = false)
+        {
+            return await GetEntities(session, includeDeleted)
+                .Where(obj => obj.EndDate == endDate)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<CourseSession> GetByStartDate(DateTime startDate, ContextSession session, bool includeDeleted = false)
+        {
+            return await GetEntities(session, includeDeleted)
+                .Where(obj => obj.StartDate == startDate)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
