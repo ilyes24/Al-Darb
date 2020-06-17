c383db15dec6a32a65ef11b1d3e9f570cd30beb4
ilyes24 <ilyes24ghomrani@gmail.com>
2020-06-17 18:54:09 +0100

    Migrating and Updating database

--- AlDarb/AlDarb.DataAccess.EFCore/Migrations/20200617175214_PhotoToStringUrlField.Designer.cs
+++ AlDarb/AlDarb.DataAccess.EFCore/Migrations/20200617175214_PhotoToStringUrlField.Designer.cs
@@ -4,14 +4,16 @@ using AlDarb.DataAccess.EFCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AlDarb.DataAccess.EFCore.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200617175214_PhotoToStringUrlField")]
+    partial class PhotoToStringUrlField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AlDarb/AlDarb.DataAccess.EFCore/Migrations/20200617175214_PhotoToStringUrlField.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlDarb.DataAccess.EFCore.Migrations
+{
+    public partial class PhotoToStringUrlField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProgressTasks_CourseTasks_CourseTaskId",
+                schema: "starter_core",
+                table: "ProgressTasks");
+
+            migrationBuilder.DropTable(
+                name: "UserPhotos",
+                schema: "starter_core");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                schema: "starter_core",
+                table: "Users",
+                nullable: true,
+                defaultValue: "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProgressTasks_CourseTasks_CourseTaskId",
+                schema: "starter_core",
+                table: "ProgressTasks",
+                column: "CourseTaskId",
+                principalSchema: "starter_core",
+                principalTable: "CourseTasks",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProgressTasks_CourseTasks_CourseTaskId",
+                schema: "starter_core",
+                table: "ProgressTasks");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                schema: "starter_core",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "UserPhotos",
+                schema: "starter_core",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPhotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserPhotos_Users_Id",
+                        column: x => x.Id,
+                        principalSchema: "starter_core",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProgressTasks_CourseTasks_CourseTaskId",
+                schema: "starter_core",
+                table: "ProgressTasks",
+                column: "CourseTaskId",
+                principalSchema: "starter_core",
+                principalTable: "CourseTasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
